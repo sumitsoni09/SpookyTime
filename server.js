1906fe31de1283e8a4f97c80abea9ed7599c8063
Sumit Soni <sumit@sumits-mbp.home>
2018-10-26 15:33:31 -0500

    make it better

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express()
 const mongoose = require('mongoose');
 const session = require('express-session')
-
+const db = mongoose.connection;
 
 
 app.use(session({
@@ -14,6 +14,18 @@ app.use(session({
 app.use(express.json())
 app.use(express.static('public'))
 
+//extended: false - does not allow nested objects in query strings
+app.use(express.urlencoded({extended: false}));
+
+//PORT
+//allow use of Heroku's port or your own local port, depending on the enviornment
+const PORT = process.env.PORT || 3000
+
+//DATABASE
+//How to connect to the database either via heroku or locally
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/' + 'haunted';
+
+//ROUTES
 app.get('/app', (req, res)=> {
   if(req.session.currentUser) {
     res.json(req.session.currentUser)
@@ -36,12 +48,18 @@ app.use('/users', usersController)
 const placesController = require('./controllers/places.js')
 app.use('/places', placesController)
 
-
-mongoose.connect('mongodb://localhost:27017/haunted', {useNewUrlParser: true })
+//CONNECT TO MONGO
+mongoose.connect(MONGODB_URI, {useNewUrlParser: true })
 mongoose.connection.once('open', () => {
   console.log('Connected to Mongoose!')
 })
 
-app.listen(3000, () => {
-  console.log('Spooky things on port 3000...')
-})
+//ERROR / SUCCESS
+db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
+db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
+//OPEN THE CONNECTION TO MONGO
+db.on('open', () => {});
+//Listener localhost://3000 - this will reroute to index
+app.listen(PORT, () => console.log('Spooky things on port ', PORT));
